eef0e3027362f35623bfa2ee25f046623ee18b77
zildjianvitoo <zildjianvito07@gmail.com>
2024-11-25 19:38:17 +0700

    fix(documents): model documents,chat description's field types

--- app/models/documents.py
+++ app/models/documents.py
@@ -6,12 +6,14 @@ from tortoise.contrib.pydantic import pydantic_model_creator
 class Documents(models.Model):
     id = fields.IntField(pk=True)
     title = fields.CharField(max_length=255)
-    description = fields.CharField(max_length=255, null=True)
+    description = fields.TextField(null=True)
     token_identifier = fields.CharField(max_length=255, null=True)
     org_id = fields.CharField(max_length=255, null=True)
     embedding = fields.JSONField(null=True, default=None)
     file_id = fields.CharField(max_length=255)  # Assuming fileId is a string
-    user = fields.ForeignKeyField("models.Users", related_name="documents", null=True)  # Relasi ke Users
+    user = fields.ForeignKeyField(
+        "models.Users", related_name="documents", null=True
+    )  # Relasi ke Users
 
     class Meta:
         table = "documents"
