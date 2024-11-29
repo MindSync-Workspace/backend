cc40a60e9de868300a72754b83cc612493d186ee
zildjianvitoo <zildjianvito07@gmail.com>
2024-11-29 21:46:56 +0700

    docs: add todo for upload document func

--- app/controllers/document_controller.py
+++ app/controllers/document_controller.py
@@ -11,14 +11,15 @@ DocumentPydantic = pydantic_model_creator(Documents, name="Document")
 
 class DocumentController:
 
-    async def create_document(self, document_data: DocumentCreate):
+    async def upload_document(self, document_data: DocumentCreate):
         try:
+            ## TODO: handle FileUpload, handleEncrypt (use cryptography package)
             doc_dict = document_data.model_dump()
             doc_obj = await Documents.create(**doc_dict)
             doc_data = await DocumentPydantic.from_tortoise_orm(doc_obj)
             return create_response(
                 status_code=status.HTTP_201_CREATED,
-                message="Document berhasil dibuat",
+                message="Document berhasil diupload",
                 data=doc_data.model_dump(),
             )
         except Exception as e:
